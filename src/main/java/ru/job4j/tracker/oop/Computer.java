1fd0b5b70886992e395292b649d084f0270518a0
SorokinYar <Sorokin.yar@yandex.ru>
2024-10-01 12:44:19 -0600

    1.7. Конструктор [#173889 #22463]

--- src/main/java/ru/job4j/tracker/oop/Computer.java
+++ src/main/java/ru/job4j/tracker/oop/Computer.java
@@ -15,9 +15,20 @@ public class Computer {
         System.out.println("SSD " + ssd + "GB");
         System.out.println(" Модель  CPU " + cpu);
     }
+
+    public static void printNumber(int x) {
+        for (int i = 0;i <= x; i++) {
+            System.out.println(i);
+        }
+    }
     public static void main(String[] args) {
         Computer computer = new Computer();
         computer.printInfo();
+        printNumber(7);
     }
+
+
+
+
 
 }
